0a2f0e32f13d95cf04ba76879a7dcbbbdb032971
MikeLai <mike.lai@skylinetw.com>
2019-04-29 10:10:32 +0800

    Initial Commit

---
+++ KKAPI_Demo_Mike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KKAPI_Demo_Mike
+//
+//  Created by Mike Lai on 2019/4/29.
+//  Copyright © 2019 Mike.Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
