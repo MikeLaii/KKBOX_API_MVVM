4235ddc7d668ea51bcbbfb77484a2b0b0f6708a6
MikeLai <mike.lai@skylinetw.com>
2019-05-22 10:43:23 +0800

    add protocol

---
+++ KKAPI_Demo_Mike/ViewControllerProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerProtocol.swift
+//  KKAPI_Demo_Mike
+//
+//  Created by Mike Lai on 2019/5/21.
+//  Copyright © 2019 Mike.Lai. All rights reserved.
+//
+
+import UIKit
+
+protocol ViewControllerProtocol {
+    func initial()
+    func binding()
+}
